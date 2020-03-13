5f2775e41b26b08c4ebb752c7bb6f208de891245
EmperorJato <59302362+EmperorJato@users.noreply.github.com>
2020-03-13 17:51:48 +0800

    Update

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('app/public'),
+            'root' => public_path('app/public/storage/attachments'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
