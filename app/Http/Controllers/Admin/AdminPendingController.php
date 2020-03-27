29072dad1e3a5a97cc7a7f89ea0490d041e0410a
EmperorJato <59302362+EmperorJato@users.noreply.github.com>
2020-03-27 17:58:11 +0800

    Update

--- app/Http/Controllers/Admin/AdminPendingController.php
+++ app/Http/Controllers/Admin/AdminPendingController.php
@@ -19,7 +19,7 @@ class AdminPendingController extends Controller
 
     public function index(){
 
-        $prform = Attachment::select('prforms.*', 'attachments.attachment_id')->rightJoin('prforms', 'prforms.pr_id', 'attachments.attachment_id')->where('status', 'Requested')->distinct('attachments.attachment_id')->orderBy('series_no', 'asc')->paginate(30);
+        $prform = PRForms::select('prforms.*', 'attachments.attachment_id')->leftJoin('attachments', 'attachments.attachment_id', 'prforms.pr_id')->where('status', 'Requested')->distinct('attachments.attachment_id')->orderBy('series_no', 'asc')->paginate(30);
         
         return view('admin.admin-pending', compact('prform'));
 
