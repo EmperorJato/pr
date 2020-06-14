fb2229fdcfc86649142b15a23ed889d734d8652e
EmperorJato <59302362+EmperorJato@users.noreply.github.com>
2020-06-15 01:02:53 +0800

    Update

--- resources/views/admin/admin-account.blade.php
+++ resources/views/admin/admin-account.blade.php
@@ -97,7 +97,7 @@
 </div>
 
 <div class="modal fade bd-example-modal-lg" id="modalProducts" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
+  <div class="modal-dialog modal-dialog-scrollable modal-xl">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Products</h5>
